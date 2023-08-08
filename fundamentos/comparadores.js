3a281dda4323a9762852a6f5bf26af708c17144d
EDINALDO BARROS <115654611+ergbarros@users.noreply.github.com>
2023-08-07 22:08:05 -0300

    Add files via upload

---
+++ fundamentos/comparadores.js
@@ -0,0 +1,12 @@
+/**
+ * Comparadores
+ */
+
+console.clear
+console.log("operadores comparativos")
+console.log(">   | maior que")
+console.log(">=  | maior ou igual a")
+console.log("<   | menor que")
+console.log("<   | menor igual a")
+console.log("=== | idêntico a")
+console.log("!== | não idênticos a")
---
+++ fundamentos/operadoreslogicos.js
@@ -0,0 +1,10 @@
+/**
+ * Estudos dos Operadores Lógicos
+ * https://professorjosedeassis.github.io/simulador/
+ */
+
+console.clear
+console.log("operadores Lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("!  | NOT")
