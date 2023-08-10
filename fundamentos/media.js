35508aa65f8976eb91f3a6fce19e64e85ddee9d7
EDINALDO BARROS <115654611+ergbarros@users.noreply.github.com>
2023-08-09 22:20:03 -0300

    Add files via upload

---
+++ fundamentos/media.js
@@ -0,0 +1,30 @@
+/**
+ * App média para calcular a média de 2 notas
+ */
+// variáveis
+let nome, diciplina
+let nota1, nota2, media 
+
+console.clear
+console.log(" ___      _     _   _ ")      
+console.log("| _ ) ___| |___| |_(_)_ __ ")  
+console.log("| _ \\/ _ \\ / -_)  _| | '  \\ ")
+console.log("|___/\\___/_\\___|\\__|_|_|_|_| ")
+console.log("")                                            
+
+                            
+// entrada
+nome = "Edinaldo"
+disciplina = "Matemática"
+nota1 = 9
+nota2 = 3
+
+// Processamneto
+media = (nota1 + nota2) / 2
+
+// Saída
+console.log(`Nome: ${nome}`)
+console.log(`Disciplina: ${disciplina}`)
+console.log(`nota 1: ${nota1}`)
+console.log(`nota 2: ${nota2}`)
+console.log(`media final: ${media}`)
