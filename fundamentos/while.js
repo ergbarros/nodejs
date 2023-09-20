6ff6a5faf4ab670b5929b209325f5d3aaa4d145c
EDINALDO BARROS <115654611+ergbarros@users.noreply.github.com>
2023-09-20 20:29:00 -0300

    Add files via upload

---
+++ fundamentos/while.js
@@ -0,0 +1,20 @@
+/**
+ * Estudos das estrutura de controle 
+ * while | do while
+ */
+
+const read = require('readline-sync')
+let x = 1
+let y = 1
+
+while (x < 11 ) {
+    console.log("teste da estrutura while")
+    x++
+}
+read.question("Pressione a tecla [ Enter } para continuar")
+console.clear()
+
+ do {
+    console.log("teste teste da estrutura do while")
+    y++
+ } while (y < 11)
