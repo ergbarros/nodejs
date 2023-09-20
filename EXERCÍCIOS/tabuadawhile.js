43fef414af64f9acb833c617214cb30267c8b09a
EDINALDO BARROS <115654611+ergbarros@users.noreply.github.com>
2023-09-20 20:28:07 -0300

    Add files via upload

---
+++ EXERCÍCIOS/tabuadawhile.js
@@ -0,0 +1,14 @@
+/**
+ * Exemplo de estrutura while 
+ * while | do while
+ */
+
+
+let valor = 7
+let i = 1
+
+while (i < 11 ) {
+    //console.log("teste da estrutura while")
+    console.log(`${valor} x ${i} = ${i * valor}`)
+    i++
+}
